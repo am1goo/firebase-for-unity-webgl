45b7b6fdd6ed3def43c4c44a02216e13b46ea1de
Pavel Shestakov <0xShestakov@gmail.com>
2026-02-22 10:37:56 +0300

    added references of existing modules into IFirebaseApp

--- Runtime/Modules/App/IFirebaseApp.cs
+++ Runtime/Modules/App/IFirebaseApp.cs
@@ -2,6 +2,10 @@ namespace FirebaseWebGL
 {
     public interface IFirebaseApp : IFirebaseModule
     {
-
+        IFirebaseAnalytics Analytics { get; }
+        IFirebaseMessaging Messaging { get; }
+        IFirebaseRemoteConfig RemoteConfig { get; }
+        IFirebaseInstallations Installations { get; }
+        IFirebasePerformance Performance { get; }
     }
 }
